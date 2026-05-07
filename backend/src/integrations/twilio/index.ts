61e884c8d17d5317b5f870f6059c7f56c74c88a4
RONALD <ronaldsilvajob@yahoo.com>
2026-05-07 00:41:05 -0300

    feat(twilio): add barrel export for Twilio integration module

---
+++ backend/src/integrations/twilio/index.ts
@@ -0,0 +1,5 @@
+export { twilioClient } from './client';
+export { sendMessage, sendTemplate, sendMediaMessage } from './sender';
+export { TEMPLATES } from './templates';
+export type { TemplateName } from './templates';
+export { default as twilioWebhookRouter } from './webhook';
